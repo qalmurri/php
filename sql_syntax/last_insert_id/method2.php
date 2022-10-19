0ce547369dcd7f4ff8573f928170654d38c1b030
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-10-19 22:51:25 +0700

    Create method2.php

---
+++ sql_syntax/last_insert_id/method2.php
@@ -0,0 +1,5 @@
+$sql = 'INSERT INTO table1 (sarah, dino) VALUES (0,0)
+SET @last_id= LAST_INSERT_ID();
+INSERT INTO table2 (id, johnson) VALUES(@last_id,?);'
+$row = $config->prepare($sql);
+$row->execute($data);
