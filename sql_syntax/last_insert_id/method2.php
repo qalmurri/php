bcfcd7f95d40764032a50b79efdb416e6165a7c3
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-10-19 22:51:50 +0700

    Update method2.php

--- sql_syntax/last_insert_id/method2.php
+++ sql_syntax/last_insert_id/method2.php
@@ -1,5 +1,7 @@
+<?php
 $sql = 'INSERT INTO table1 (sarah, dino) VALUES (0,0)
 SET @last_id= LAST_INSERT_ID();
 INSERT INTO table2 (id, johnson) VALUES(@last_id,?);'
 $row = $config->prepare($sql);
 $row->execute($data);
+?>
